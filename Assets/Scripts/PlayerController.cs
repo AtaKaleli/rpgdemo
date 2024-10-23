0de336849818509e4610f07f432aa523c50d5619
ata kaleli <atakaleli123@gmail.com>
2024-10-23 14:16:44 +0300

    Player Idle/Move anim and flip is added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,8 +9,8 @@ public class PlayerController : MonoBehaviour
     private PlayerControls playerControls;
     private Vector2 movement;
     private Rigidbody2D rb;
-
-
+    private Animator anim;
+    private SpriteRenderer sr;
 
 
 
@@ -20,6 +20,8 @@ public class PlayerController : MonoBehaviour
     {
         playerControls = new PlayerControls();
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
     }
 
     private void OnEnable()
@@ -29,13 +31,24 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
+        
         PlayerInput();
+        AnimationController();
     }
 
     private void FixedUpdate()
     {
+        FlipController();
         Move();
     }
+
+    private void AnimationController()
+    {
+        anim.SetFloat("xVelocity", movement.x);
+        anim.SetFloat("yVelocity", movement.y);
+    }
+
+
 
     private void PlayerInput()
     {
@@ -46,5 +59,22 @@ public class PlayerController : MonoBehaviour
     {
         rb.MovePosition(rb.position + movement * (moveSpeed * Time.fixedDeltaTime));
 
+    }
+
+    private void FlipController()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 playerScreenPoint = Camera.main.WorldToScreenPoint(transform.position);
+        
+        FlipChecks(mousePos, playerScreenPoint);
+
+    }
+
+    private void FlipChecks(Vector3 mousePos, Vector3 playerScreenPoint)
+    {
+        if (mousePos.x < playerScreenPoint.x)
+            sr.flipX = true;
+        else
+            sr.flipX = false;
     }
 }
